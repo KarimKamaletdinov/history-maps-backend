440b7d86daaad3084794ae8cf2a2b72c9955ebe3
karim <450906k@gmail.com>
2022-05-15 10:41:48 +0500

    to push

---
+++ HistoryMaps.Stl.Abstractions/Test.cs
@@ -0,0 +1,15 @@
+﻿using IxMilia.Stl;
+
+namespace HistoryMaps;
+
+public class Repository
+{
+	public void W()
+	{
+		StlFile stlFile;
+		using (FileStream fs = new FileStream(@"C:\Path\To\File.stl", FileMode.Open))
+		{
+			stlFile = StlFile.Load(fs);
+		}
+	}
+}
